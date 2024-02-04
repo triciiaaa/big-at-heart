28de6925784bedc890b7935810ed2d80eb16eaae
triciiaaa <angjeanee@gmail.com>
2024-02-04 21:59:16 +0800

    Setup navigation routes

---
+++ src/pages/Achievements.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar'
+
+const Achievements = () => {
+  return (
+    <>
+        <Navbar />
+        <div>Achievements</div>
+    </>
+  )
+}
+
+export default Achievements
---
+++ src/pages/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar'
+
+const Blog = () => {
+  return (
+    <>
+        <Navbar />
+        <div>Blog</div>
+    </>
+  )
+}
+
+export default Blog
