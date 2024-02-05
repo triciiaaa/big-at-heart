bea0649ded581d09972a797bd937903cdd6abd53
triciiaaa <angjeanee@gmail.com>
2024-02-05 23:19:41 +0800

    Edit Blog and Profile page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 
 import Navbar from "../components/Navbar";
-import blog1 from "../graphics/blog-1.jpg"
-import blog2 from "../graphics/blog-2.jpg"
-import blog3 from "../graphics/blog-3.jpg"
-import blog4 from "../graphics/blog-4.jpg"
+import blog1 from "../graphics/blog-1.jpg";
+import blog2 from "../graphics/blog-2.jpg";
+import blog3 from "../graphics/blog-3.jpg";
+import blog4 from "../graphics/blog-4.jpg";
+
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 import {
   Typography,
@@ -26,28 +28,28 @@ const Blog = () => {
       title: "Making a Difference: Volunteering in Your Local Community",
       content:
         "In this blog post, we explore the impact of volunteering within your local community. Discover how small acts of kindness and dedicated volunteer work can make a significant difference in the lives of others. From organizing community events to supporting local charities, find inspiration and learn about the fulfilling experiences that come with giving back.",
-      image: blog1
+      image: blog1,
     },
     {
       id: 2,
       title: "Empowering Youth Through Volunteerism",
       content:
         "This blog delves into the power of involving young people in volunteer activities. Explore the various ways in which volunteering can positively influence the personal development of youth, providing them with valuable skills, fostering a sense of responsibility, and building a foundation for a lifetime of community engagement. Join us as we discuss the transformative impact of empowering the next generation through volunteerism.",
-      image: blog2
+      image: blog2,
     },
     {
       id: 3,
       title: "Environmental Stewardship: Volunteering for a Greener Tomorrow",
       content:
         "Dive into the world of environmental volunteering and learn how individuals can contribute to a sustainable future. From tree planting initiatives to beach cleanups, discover the various ways you can actively participate in environmental stewardship. Explore the rewards of connecting with nature and making a positive impact on the planet through volunteer efforts.",
-      image: blog3
+      image: blog3,
     },
     {
       id: 4,
       title: "Virtual Volunteering: Making a Global Impact from Anywhere",
       content:
         "This blog explores the concept of virtual volunteering, showcasing how individuals can contribute to causes worldwide without leaving their homes. Discover online platforms and initiatives that allow you to volunteer your time, skills, and expertise remotely. Explore the opportunities for creating positive change in diverse communities, all from the comfort of your own space.",
-      image: blog4
+      image: blog4,
     },
   ];
 
@@ -71,20 +73,30 @@ const Blog = () => {
         <Typography variant="h4" fontWeight="bold" gutterBottom>
           Blog
         </Typography>
-        <Typography variant="h6" gutterBottom>
-          Recent Blogs  
-        </Typography>
+        <Stack direction="row" justifyContent="space-between">
+          <Typography variant="h6" gutterBottom>
+            Recent Blogs
+          </Typography>
+          <FilterListIcon />
+        </Stack>
         <List>
           <Stack direction="row">
             {existingBlogs.map((blog) => (
               <ListItem key={blog.id}>
-                <Card sx={{ width: 300, height: 500 }}>
+                <Card sx={{ width: 300, height: 530 }}>
                   <CardContent>
-                  <img src={blog.image} alt={blog.title} style={{ maxWidth: '100%', marginBottom: '16px' }} />
+                    <img
+                      src={blog.image}
+                      alt={blog.title}
+                      style={{ maxWidth: "100%", marginBottom: "16px" }}
+                    />
                     <ListItemText
                       primary={blog.title}
                       secondary={blog.content}
                     />
+                    <Button variant="text" onClick={handleCreateBlog}>
+                      Read More
+                    </Button>
                   </CardContent>
                 </Card>
               </ListItem>
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 import Navbar from "../components/Navbar";
 import profile_image from "../graphics/profile_image.png";
+import footsteps from "../graphics/footsteps.svg";
+import workshop1 from "../graphics/workshop-1.png";
+import workshop2 from "../graphics/workshop-2.png";
+import workshop3 from "../graphics/workshop-3.png";
 
 import {
   Button,
@@ -32,17 +36,25 @@ const ProfilePage = () => {
   const workshops = [
     {
       id: 1,
-      title: "Introduction to React",
+      title: "Digital Marketing Essentials",
       description:
-        "Learn the basics of React.js and build your first React app.",
+        "Explore the fundamentals of digital marketing in this workshop. Learn the basics of social media management, content creation, and online advertising. Acquire practical skills to enhance your organization's online presence and effectively communicate its mission.",
+      image: workshop1,
     },
     {
       id: 2,
-      title: "Node.js Fundamentals",
+      title: "Web Development Bootcamp",
       description:
-        "Explore the fundamentals of Node.js for server-side JavaScript development.",
+        "Dive into the world of web development with our intensive bootcamp. Whether you're a beginner or looking to refine your skills, this workshop covers HTML, CSS, and JavaScript essentials. Participants will have the opportunity to build a basic website and contribute to enhancing their organization's online platforms.",
+      image: workshop2,
     },
-    // Add more workshops as needed
+    {
+      id: 3,
+      title: "Effective Communication for Volunteer Leaders",
+      description:
+        "Elevate your leadership skills through effective communication techniques. This workshop focuses on interpersonal communication, team collaboration, and conflict resolution. Volunteers will gain valuable insights to lead and inspire their teams, fostering a positive and productive volunteering environment.",
+      image: workshop3,
+    },
   ];
 
   return (
@@ -53,120 +65,172 @@ const ProfilePage = () => {
           Profile
         </Typography>
         <Stack direction="row" justifyContent="space-between">
-          <Card>
-            <CardContent>
-              <Stack margin="32px" gap="32px">
-                <Stack
-                  direction="row"
-                  gap="64px"
-                  alignItems="center"
-                  justifyContent="space-between"
-                >
-                  <Stack direction="row" alignItems="center" gap="24px">
-                    <img src={profile_image} alt="Logo" width="150px" />
-                    <Stack>
-                      <Typography variant="body1" component="div" fontWeight="bold" gutterBottom>
-                        NAME
-                      </Typography>
-                      <Typography variant="h4">Linda Tan</Typography>
+          <Stack gap="32px">
+            <Card sx={{ width: 450, height: 650 }}>
+              <CardContent>
+                <Stack margin="32px" gap="32px">
+                  <Stack
+                    direction="row"
+                    gap="64px"
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Stack direction="row" alignItems="center" gap="24px">
+                      <img src={profile_image} alt="Logo" width="150px" />
+                      <Stack>
+                        <Typography
+                          variant="body1"
+                          component="div"
+                          fontWeight="bold"
+                          gutterBottom
+                        >
+                          NAME
+                        </Typography>
+                        <Typography variant="h4">Linda Tan</Typography>
+                      </Stack>
                     </Stack>
                   </Stack>
-                </Stack>
-                <Box>
-                  <Typography variant="body1" color="text.secondary">
-                    Email: lindatan@gmail.com
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Handphone No: +65 94372949
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Date of Birth: 31 December 2001
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Gender: Female
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Occupation: Student
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    School: National University of Singapore (NUS)
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Educational Background:
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Availability: Mon-Thurs 4-6pm
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Driving/Non-driving: Non-driving
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Own Vehicle: No
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Commitment Level: Ad-hoc, 4 hours a week
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    Immigration Status: Singapore Citizen
-                  </Typography>
-                </Box>
-                <Button variant="contained" sx={{ bgcolor: "#CD3301", "&:hover": { bgcolor: "#992200" } }}>Edit Profile</Button>
-              </Stack>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: "400px" }}>
-            <CardContent>
-              <Typography variant="h5" fontWeight="bold" gutterBottom>
-                Recommended Workshops
-              </Typography>
-              {workshops.map((workshop) => (
-                <Stack
-                  key={workshop.id}
-                  direction="column"
-                  spacing={1}
-                  marginBottom={2}
-                >
-                  <Typography variant="h6">{workshop.title}</Typography>
-                  <Typography variant="body1" color="text.secondary">
-                    {workshop.description}
-                  </Typography>
-                  <Button variant="contained" sx={{ bgcolor: "#CD3301", "&:hover": { bgcolor: "#992200" } }}>
-                    Sign Up
+                  <Box>
+                    <Typography variant="body1" color="text.secondary">
+                      Email: lindatan@gmail.com
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Handphone No: +65 94372949
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Date of Birth: 31 December 2001
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Gender: Female
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Occupation: Student
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      School: National University of Singapore (NUS)
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Educational Background:
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Availability: Mon-Thurs 4-6pm
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Driving/Non-driving: Non-driving
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Own Vehicle: No
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Commitment Level: Ad-hoc, 4 hours a week
+                    </Typography>
+                    <Typography variant="body1" color="text.secondary">
+                      Immigration Status: Singapore Citizen
+                    </Typography>
+                  </Box>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      bgcolor: "#CD3301",
+                      "&:hover": { bgcolor: "#992200" },
+                    }}
+                  >
+                    Edit Profile
                   </Button>
                 </Stack>
-              ))}
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent>
+                <Stack margin="32px" gap="32px">
+                  <Typography variant="h5" fontWeight="bold" gutterBottom>
+                    Badges Earned
+                  </Typography>
+                  <img src={footsteps} alt="Logo" width="50px" />
+                </Stack>
+              </CardContent>
+            </Card>
+          </Stack>
+          <Card sx={{ maxWidth: "500px" }}>
+            <CardContent>
+              <Stack margin="32px">
+                <Typography variant="h5" fontWeight="bold" gutterBottom>
+                  Recommended Workshops
+                </Typography>
+                <Stack gap="32px">
+                  {workshops.map((workshop) => (
+                    <Stack
+                      key={workshop.id}
+                      direction="column"
+                      gap="16px"
+                      marginBottom={2}
+                    >
+                      <img
+                        src={workshop.image}
+                        alt="Logo"
+                        width="200px"
+                        height="200px"
+                      />
+                      <Typography variant="h6">{workshop.title}</Typography>
+                      <Typography variant="body1" color="text.secondary">
+                        {workshop.description}
+                      </Typography>
+                      <Button
+                        variant="contained"
+                        sx={{
+                          bgcolor: "#CD3301",
+                          "&:hover": { bgcolor: "#992200" },
+                        }}
+                      >
+                        Sign Up
+                      </Button>
+                    </Stack>
+                  ))}
+                </Stack>
+              </Stack>
             </CardContent>
           </Card>
-          <Card>
+          <Card sx={{ maxWidth: "350px" }}>
             <CardContent>
-              <Typography variant="h5" fontWeight="bold" gutterBottom>
-                Skills
-              </Typography>
-              <Stack direction="row" spacing={1}>
-                {skills.map((skill, index) => (
-                  <Chip
-                    key={index}
-                    label={skill}
-                    onDelete={() => handleRemoveSkill(skill)}
-                    sx={{ bgcolor: "#CD3301", "&:hover": { bgcolor: "#992200" }, color:"white"}}
+              <Stack margin="32px">
+                <Typography variant="h5" fontWeight="bold" gutterBottom>
+                  Skills
+                </Typography>
+                <Stack direction="row" gap="8px" flexWrap="wrap">
+                  {skills.map((skill, index) => (
+                    <Chip
+                      key={index}
+                      label={skill}
+                      onDelete={() => handleRemoveSkill(skill)}
+                      sx={{
+                        bgcolor: "#CD3301",
+                        "&:hover": { bgcolor: "#992200" },
+                        color: "white",
+                      }}
+                    />
+                  ))}
+                </Stack>
+                <Stack gap="32px">
+                  <TextField
+                    label="Add Skill"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    value={newSkill}
+                    onChange={(e) => setNewSkill(e.target.value)}
                   />
-                ))}
+                  <Button
+                    variant="contained"
+                    sx={{
+                      bgcolor: "#CD3301",
+                      "&:hover": { bgcolor: "#992200" },
+                    }}
+                    onClick={handleAddSkill}
+                  >
+                    Add Skill
+                  </Button>
+                </Stack>
               </Stack>
-              <TextField
-                label="Add Skill"
-                variant="outlined"
-                margin="normal"
-                fullWidth
-                value={newSkill}
-                onChange={(e) => setNewSkill(e.target.value)}
-              />
-              <Button
-                variant="contained"
-                sx={{ bgcolor: "#CD3301", "&:hover": { bgcolor: "#992200" } }}
-                onClick={handleAddSkill}
-              >
-                Add Skill
-              </Button>
             </CardContent>
           </Card>
         </Stack>
